7fde0353af5010f77d5f8c0c8f421c568e49fcce
Thomas Heller <info@zilence.net>
2015-10-19 23:36:49 +0200

    advanced compiled

--- src/dev/build.clj
+++ src/dev/build.clj
@@ -3,12 +3,25 @@
             [shadow.devtools.server :as devtools]))
 
 
-(defn dev [& args]
+(defn project-setup []
   (-> (cljs/init-state)
       (cljs/find-resources-in-classpath)
       (cljs/find-resources "src/cljs")
       (cljs/find-resources "module")
       (cljs/configure-module :test ['react-test.app] #{}
-        {:prepend "process = {env: {NODE_ENV:'not-production'}};"})
+        {:prepend-js "process = {env: {NODE_ENV:'production'}};"})
+      (cljs/finalize-config)
+      ))
+
+(defn dev [& args]
+  (-> (project-setup)
       (devtools/start-loop {})
       ))
+
+(defn production [& args]
+  (-> (project-setup)
+      (assoc :optimizations :advanced)
+      (cljs/compile-modules)
+      (cljs/closure-optimize)
+      (cljs/flush-modules-to-disk))
+  :done)
